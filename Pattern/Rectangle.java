a507ce164386069ec364fbbcc2787348d3f519b4
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2026-03-02 05:21:57 +0530

    Rectangle Pattern

---
+++ Pattern/Rectangle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Rectangle {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int rows=sc.nextInt();
+		int columns=sc.nextInt();
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<columns;j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+}
