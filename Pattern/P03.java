012e6064d99b7f1bfe882029cf353d2759f83f48
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2026-03-05 06:19:32 +0530

    Hollow Rectangle
    * * * 
    *    * 
    *    * 
    * * *

--- Pattern/P03.java
+++ Pattern/P03.java
@@ -1,11 +1,12 @@
 import java.util.*;
-public class P3 {
+public class P03 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<n;j++) {
-				if(i==0||i==n-1||j==0||j==n-1)
+		int rows=sc.nextInt();
+		int columns=sc.nextInt();
+		for(int i=0;i<rows;i++) {
+			for(int j=0;j<columns;j++) {
+				if(i==0||i==rows-1||j==0||columns==n-1)
 					System.out.print("* ");
 				else
 					System.out.print("  ");
